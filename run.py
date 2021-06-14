14716bffdad09d6f4420c407e83460d2f5c96a6d
PureSnowFox <mengshouer@gmail.com>
2021-06-14 23:50:40 +0800

    update

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 from apscheduler.schedulers.blocking import BlockingScheduler
 from Checkin52pj.Checkin52pjForSCF import pjCheckin as pj
 from Cloud189Checkin.C189CheckinForSCF import C189Checkin as c189
@@ -10,52 +11,63 @@ from smzdmCheckin.smzdmCheckinForSCF import smzdm_pc as smzdm
 from V2EX.v2ex import main as v2ex
 from Zhiyou.zhiyou import main as zhiyou
 
-username = os.environ.get("username")
-password = os.environ.get("password")
-fflogin_name = os.environ.get("fflogin_name")
-fflogin_password = os.environ.get("fflogin_password")
-area_name = os.environ.get("area_name")
-server_name = os.environ.get("server_name")
-role_name = os.environ.get("role_name")
-cookie_pj = os.environ.get("cookie_52pj")
-cookie_smzdm = os.environ.get("cookie_smzdm")
-netease_username = os.environ.get("netease_username")
-netease_password = os.environ.get("netease_password")
-note_username = os.environ.get("note_username")
-note_password = os.environ.get("note_password")
-cookie_v2ex = os.environ.get("cookie_v2ex")
-cookie_enshan = os.environ.get("cookie_enshan")
-cookie_zhiyou = os.environ.get("cookie_zhiyou")
+with open('/etc/config/config.yaml', encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+try:
+    os.environ["username"] = data["username"]
+    os.environ["password"] = data["password"]
+except:
+    print("未设置天翼云配置")
+try:
+    os.environ["fflogin_name"] = data["fflogin_name"]
+    os.environ["fflogin_password"] = data["fflogin_password"]
+    os.environ["area_name"] = data["area_name"]
+    os.environ["server_name"] = data["server_name"]
+    os.environ["role_name"] = data["role_name"]
+except:
+    print("未设置FF14配置")
+try:
+    os.environ["cookie_52pj"] = data["cookie_52pj"]
+except:
+    print("未设置52pojie配置")
+try:
+    os.environ["cookie_smzdm"] = data["cookie_smzdm"]
+except:
+    print("未设置什么值得买配置")
+try:
+    os.environ["netease_username"] = data["netease_username"]
+    os.environ["netease_password"] = data["netease_password"]
+except:
+    print("未设置网易云音乐配置")
+try:
+    os.environ["note_username"] = data["note_username"]
+    os.environ["note_password"] = data["note_password"]
+except:
+    print("未设置有道云笔记配置")
+try:
+    os.environ["cookie_v2ex"] = data["cookie_v2ex"]
+except:
+    print("未设置v2ex配置")
+try:
+    os.environ["cookie_enshan"] = data["cookie_enshan"]
+except:
+    print("未设置恩山论坛配置")
+try:
+    os.environ["cookie_zhiyou"] = data["cookie_zhiyou"]
+except:
+    print("未设置智友邦配置")
 
 def job():
     print("run start")
-    if username and password:
-        print("1")
-        c189()
-    if fflogin_name and fflogin_password and area_name and server_name and role_name:
-        print("2")
-        ff14()
-    if cookie_pj:
-        print("3")
-        pj()
-    if cookie_smzdm:
-        print("4")
-        smzdm()
-    if netease_username and netease_password:
-        print("5")
-        netease_music()
-    if note_username and note_password:
-        print("6")
-        noteyoudao()
-    if cookie_v2ex:
-        print("7")
-        v2ex()
-    if cookie_enshan:
-        print("8")
-        enshan()
-    if cookie_zhiyou:
-        print("9")
-        zhiyou()
+    c189()
+    ff14()
+    pj()
+    smzdm()
+    netease_music()
+    noteyoudao()
+    v2ex()
+    enshan()
+    zhiyou()
     print("run end")
 
 if __name__ == "__main__":
